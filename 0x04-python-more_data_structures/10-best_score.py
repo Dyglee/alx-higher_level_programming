b5b23b8777c5aa376c60e64aec7e2af0c90914fd
Yassine <yassinemoussafir31@gmail.com>
2024-01-10 07:23:56 -0800

    t

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
 
     maxkey = 0
     maxvalue = 0
     for key, value in a_dictionary.items():
         if value > maxvalue:
-            maxvalue = value 
+            maxvalue = value
             maxkey = key
     return maxkey
