6eb2aa126ed3a9facbad2cff529aaaf0902fe188
Yassine <yassinemoussafir31@gmail.com>
2024-01-10 04:31:53 -0800

    square

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    newmatrix = []
+    for row in matrix:
+        newrow = []
+        for i in row:
+            newrow.append( i **2 )
+        newmatrix.append(newrow)
+    return newmatrix
