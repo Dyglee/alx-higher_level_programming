a34f3f4f58625048b8766166e15b83c5edeac6eb
Yassine <yassinemoussafir31@gmail.com>
2024-01-22 04:15:22 -0800

    logic

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 def safe_print_list(my_list=[], x=0):
-    list_len = 0
-
-    try:
-        for i in range(x):
+    counter = 0
+    
+    for i in range(0, x):
+        try:
             print("{}".format(my_list[i]), end="")
-            list_len += 1
-    except IndexError:
-        pass
+            counter += 1
+        except IndexError:
+            continue
+    
     print()
-    return list_len
+    return counter
