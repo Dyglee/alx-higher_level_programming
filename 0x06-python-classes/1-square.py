03718de0bb0d8d8e6ec3f94b5d03aafc5f551fd9
Yassine <yassinemoussafir31@gmail.com>
2024-01-23 08:09:50 -0800

    1square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Square class with private instance attribute
+"""
+
+
+class Square:
+    """ representation of the square """
+    def __init__(self, size):
+        self.__size = size
