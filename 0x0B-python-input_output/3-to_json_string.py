5642e0f65d2e14b7af8aac43ea4eb01ebd935f57
Yassine <yassinemoussafir31@gmail.com>
2024-02-06 05:58:38 -0800

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-import json
+
 
 def to_json_string(my_obj):
-    """Return the JSON representation of an object.
-
-    Args:
-        my_obj (obj): The Python object to be serialized to JSON.
-
-    Returns:
-        str: A JSON-formatted string representing the input object.
-    """
+    """Return the JSON representation of object """
     return json.dumps(my_obj)
